9a383f868b886a2a438319056ac0417e5a5ca1de
Lauriane Mollier <laurianemollier@Laurianes-MacBook-Pro.local>
2024-09-16 08:27:17 +0200

    run test sequencially

--- src/__tests__/e2e/users.test.ts
+++ src/__tests__/e2e/users.test.ts
@@ -19,24 +19,4 @@ describe("/api/v1/users", () => {
     // Check if the response body is an empty array
     expect(response.body).toStrictEqual([]);
   });
-
-  it("should create the user", async (): Promise<void> => {
-    const response = await request(app).post("/api/v1/users").send({
-      name: "Adam",
-      email: "adam123@example.com",
-      password: "pasdddassword", // Correct case
-    });
-
-    console.log("Response Body:", response.body); // Log the response body for debugging
-    console.log("Response Status:", response.status); // Log the response status for debugging
-
-    // Check if the response status is 201 Created
-    expect(response.status).toBe(201);
-
-    // Validate the structure of the response body if needed
-    expect(response.body).toHaveProperty("id");
-    expect(response.body).toHaveProperty("name", "Adam");
-    expect(response.body).toHaveProperty("email", "adam123@example.com");
-    expect(response.body).toHaveProperty("role", "user");
-  });
 });
